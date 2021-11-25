b36a1fda14f85c00499fe051f1ac86e1fbfe19f5
TarKz06 <khematat_s@elearning.cmu.ac.th>
2021-11-25 19:48:16 +0700

    activity 25/11

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
@@ -19,19 +22,32 @@ class MyApp extends StatelessWidget {
         ));
   }
 }
+
 class Home extends StatefulWidget {
   // const Home({ Key? key }) : super(key: key);
 
   @override
   _HomeState createState() => _HomeState();
 }
+
 class _HomeState extends State<Home> {
+  TextEditingController quqntity = TextEditingController();
+  double price = 10000;
+  TextEditingController result = TextEditingController();
+
+  @override
+  void initState() {
+    //TODO inplement initState
+    super.initState();
+    result.text = "Buy finger ";
+  }
+
   @override
   Widget build(BuildContext context) {
     return ListView(
       children: [
         Center(
-          child: Padding(
+            child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
@@ -47,9 +63,20 @@ class _HomeState extends State<Home> {
                 "Calcutor Program",
               ),
               TextField(
+                  controller: quqntity,
                   decoration: InputDecoration(
                       labelText: "Apple Amount", border: OutlineInputBorder())),
-              ElevatedButton(onPressed: () {}, child: Text("Calcurator")),
+              ElevatedButton(
+                  onPressed: () {
+                    var cal = double.parse(quqntity.text) * price;
+                    print(
+                        "finger quality : ${quqntity.text} Total : ${cal} BTHB");
+
+                    setState(() {
+                      result.text = "buy ${quqntity.text} finger";
+                    });
+                  },
+                  child: Text("Calcurator")),
               Text("Buy 5 apple,Bucause each apple cost 100 THB ,ToTal 500 THB")
             ],
           ),
